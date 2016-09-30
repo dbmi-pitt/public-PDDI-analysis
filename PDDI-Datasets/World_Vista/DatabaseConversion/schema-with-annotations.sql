120f4ac61a895feca765d9629003a091bfefe5c7
rkboyce <codehop.dev@gmail.com>
2016-09-30 16:27:12 -0400

    slight tweaks while testing

--- PDDI-Datasets/World_Vista/DatabaseConversion/schema-with-annotations.sql
+++ PDDI-Datasets/World_Vista/DatabaseConversion/schema-with-annotations.sql
@@ -60,13 +60,13 @@ CREATE TABLE Drug_Interaction_With_Annotations (
     PRIMARY KEY (Drug_Interaction_ID)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\eng_groups2015_xml_v3.tsv' INTO TABLE Drug_Group FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Name, RxNorm, Source_File, Clinical_Source, Class_Name, Class_Code);
+LOAD DATA LOCAL INFILE './eng_groups2015_xml_v3.tsv' INTO TABLE Drug_Group FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Name, RxNorm, Source_File, Clinical_Source, Class_Name, Class_Code);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\eng_groups_tables_2015_xml_v3_v5_atc_codes.tsv' INTO TABLE ATC_Code FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (ATC_Code, Drug_Name);
+LOAD DATA LOCAL INFILE './eng_groups_tables_2015_xml_v3_v5_atc_codes.tsv' INTO TABLE ATC_Code FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (ATC_Code, Drug_Name);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\eng_tables2015-xml-v5.tsv' INTO TABLE Drug_Interaction FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Drug_1_Name, Drug_1_RxCUI, Drug_1_Class_Name, Drug_1_Code, Drug_2_Name, Drug_2_RxCUI, Drug_2_Class_Name, Drug_2_Code, Clinical_Source, Source_File, Description, Severity, `Comment`);
+LOAD DATA LOCAL INFILE './eng_tables2015-xml-v5.tsv' INTO TABLE Drug_Interaction FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Drug_1_Name, Drug_1_RxCUI, Drug_1_Class_Name, Drug_1_Code, Drug_2_Name, Drug_2_RxCUI, Drug_2_Class_Name, Drug_2_Code, Clinical_Source, Source_File, Description, Severity, `Comment`);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\eng_tables2015-xml-v5.tsv' INTO TABLE Drug_Interaction_With_Annotations FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Drug_1_Name, Drug_1_RxCUI, Drug_1_Class_Name, Drug_1_Code, Drug_2_Name, Drug_2_RxCUI, Drug_2_Class_Name, Drug_2_Code, Clinical_Source, Source_File, Description, Severity, `Comment`);
+LOAD DATA LOCAL INFILE './eng_tables2015-xml-v5.tsv' INTO TABLE Drug_Interaction_With_Annotations FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Drug_1_Name, Drug_1_RxCUI, Drug_1_Class_Name, Drug_1_Code, Drug_2_Name, Drug_2_RxCUI, Drug_2_Class_Name, Drug_2_Code, Clinical_Source, Source_File, Description, Severity, `Comment`);
 
 CREATE TABLE tmp (
     Drug_Interaction_ID INT NOT NULL,
@@ -90,49 +90,49 @@ CREATE TABLE tmp (
     PRIMARY KEY (Drug_Interaction_ID)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_consequences_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Clinical_Consequence_Annotations);
+LOAD DATA LOCAL INFILE './drug_id_and_description_consequences_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Clinical_Consequence_Annotations);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Clinical_Consequence_Annotations = VALUES(Clinical_Consequence_Annotations);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_mgmt_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Management_Option_Annotations);
+LOAD DATA LOCAL INFILE './drug_id_and_description_mgmt_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Management_Option_Annotations);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Management_Option_Annotations = VALUES(Management_Option_Annotations);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_typos_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Suspected_Typo_Annotations);
+LOAD DATA LOCAL INFILE './drug_id_and_description_typos_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Suspected_Typo_Annotations);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Suspected_Typo_Annotations = VALUES(Suspected_Typo_Annotations);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_anotes_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Annotator_Notes);
+LOAD DATA LOCAL INFILE './drug_id_and_description_anotes_1.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Annotator_Notes);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Annotator_Notes = VALUES(Annotator_Notes);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_consequences_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Clinical_Consequence_Annotations);
+LOAD DATA LOCAL INFILE './drug_id_and_description_consequences_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Clinical_Consequence_Annotations);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Clinical_Consequence_Annotations = VALUES(Clinical_Consequence_Annotations);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_mgmt_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Management_Option_Annotations);
+LOAD DATA LOCAL INFILE './drug_id_and_description_mgmt_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Management_Option_Annotations);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Management_Option_Annotations = VALUES(Management_Option_Annotations);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_typos_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Suspected_Typo_Annotations);
+LOAD DATA LOCAL INFILE './drug_id_and_description_typos_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Suspected_Typo_Annotations);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
 ON DUPLICATE KEY UPDATE Drug_Interaction_ID = VALUES(Drug_Interaction_ID), Suspected_Typo_Annotations = VALUES(Suspected_Typo_Annotations);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Documents\\GitHub\\public-PDDI-analysis\\PDDI-Datasets\\World_Vista\\DatabaseConversion\\drug_id_and_description_anotes_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Annotator_Notes);
+LOAD DATA LOCAL INFILE './drug_id_and_description_anotes_2.tsv' REPLACE INTO TABLE tmp FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Annotator_Notes);
 
 INSERT INTO Drug_Interaction_With_Annotations
 SELECT * FROM tmp
--- PDDI-Datasets/ndf-rt-world-vista-overlap/ndf-rt-schema.sql
+++ PDDI-Datasets/ndf-rt-world-vista-overlap/ndf-rt-schema.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS NDF_RT_Interaction CASCADE;
+DROP TABLE IF EXISTS NDF_RT_INTERACTION CASCADE;
 
-CREATE TABLE NDF_RT_Interaction (
-    Drug_Interaction_ID INT NOT NULL,
-    Drug_1_VUID VARCHAR(50),
-    Drug_1_RxCUI VARCHAR(50),
-    Severity VARCHAR(250),
-    Drug_2_VUID VARCHAR(50),
-    Drug_2_RxCUI VARCHAR(50),
-    PRIMARY KEY (Drug_Interaction_ID)
+CREATE TABLE NDF_RT_INTERACTION (
+    drug_interaction_id INT NOT NULL,
+    drug_1_vuid VARCHAR(50),
+    drug_1_rxcui VARCHAR(50),
+    severity VARCHAR(250),
+    drug_2_vuid VARCHAR(50),
+    drug_2_rxcui VARCHAR(50),
+    PRIMARY KEY (drug_interaction_id)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Eric\\Desktop\\ndf-rt\\NDF-RT-interactions.csv' INTO TABLE NDF_RT_Interaction FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (Drug_Interaction_ID, Drug_1_VUID, Drug_2_VUID, Severity, Drug_1_RxCui, Drug_2_RxCui);
+LOAD DATA LOCAL INFILE './NDF-RT-interactions.csv' INTO TABLE NDF_RT_INTERACTION FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (drug_interaction_id, drug_1_vuid, drug_2_vuid, severity, drug_1_rxcui, drug_2_rxcui);
 
-UPDATE ndf_rt_interaction
+UPDATE NDF_RT_INTERACTION
 SET drug_2_rxcui = TRIM(TRAILING '\r' FROM drug_2_rxcui);
 
 SET SQL_SAFE_UPDATES=0;
 
-UPDATE ndf_rt_interaction
+UPDATE NDF_RT_INTERACTION
 SET drug_1_rxcui = NULL
 WHERE drug_1_rxcui = 'NULL';
 
-UPDATE ndf_rt_interaction
+UPDATE NDF_RT_INTERACTION
 SET drug_2_rxcui = NULL
 WHERE drug_2_rxcui = 'NULL';
