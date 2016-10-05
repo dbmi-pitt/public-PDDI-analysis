716f87406e0a4a0dcdc20bb802e37d915cd77d7c
Eric Chou <echou53@gmail.com>
2016-10-04 20:22:44 -0400

    Query accounts for world vista drug classes
    View created is a left join of world vista drug interactions with drug group to account for drug classes of drug 1 and/or drug 2 in the interaction.
    
    Then, this view is joined with the NDF-RT interactions to account for all permutations of where drug classes may arise. Query takes a long time to fetch, however.

--- PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
+++ PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
@@ -1,8 +1,20 @@
-SELECT w.drug_1_rxcui AS world_vista_drug_1,
-       w.drug_2_rxcui AS world_vista_drug_2,
-       n.drug_1_rxcui AS ndf_rt_drug_1,
-       n.drug_2_rxcui AS ndf_rt_drug_2
-FROM drug_interaction w
-INNER JOIN ndf_rt_interaction n
-ON (w.drug_1_rxcui = n.drug_1_rxcui AND w.drug_2_rxcui = n.drug_2_rxcui);
+CREATE VIEW drug_class_interaction AS (SELECT g.Drug_Name AS Drug_Name_1,
+       g.RxNorm AS Drug_1_RxNorm,
+       g.Class_Code AS Class_Code_1,
+       h.Drug_Name AS Drug_Name_2,
+       h.RxNorm AS Drug_2_RxNorm,
+       h.Class_Code AS Class_Code_2,
+       i.*
+FROM drug_interaction i
+LEFT JOIN drug_group g
+ON (i.Drug_1_Code = TRIM(TRAILING '\r' FROM g.Class_Code))
+LEFT JOIN drug_group h
+ON (i.Drug_2_Code = TRIM(TRAILING '\r' FROM h.Class_Code)));
 
+SELECT *
+FROM drug_class_interaction w
+INNER JOIN NDF_RT_INTERACTION n
+ON ((w.Drug_1_RxCUI = n.drug_1_rxcui AND w.Drug_2_RxCUI = n.drug_2_rxcui)
+	OR (w.Drug_1_RxNorm = n.drug_1_rxcui AND w.Drug_2_RxCUI = n.drug_2_rxcui)
+	OR (w.Drug_1_RxCUI = n.drug_1_rxcui AND w.Drug_2_RxNorm = n.drug_2_rxcui)
+    OR (w.Drug_1_RxNorm = n.drug_1_rxcui AND w.Drug_2_RxNorm = n.drug_2_rxcui));
