e5656ce7179d74cbe17d752bb92c528b4b7e5950
Eric Chou <echou53@gmail.com>
2016-10-26 12:42:47 -0400

    Drug_Class_Interaction now table rather than view

--- PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
+++ PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
@@ -1,37 +1,42 @@
-CREATE VIEW Drug_Class_Interaction AS (
-SELECT g.Drug_Name AS Drug_Name_1,
-       g.RxNorm AS Drug_1_RxNorm,
+DROP TABLE IF EXISTS Drug_Class_Interaction;
+
+# Temporary table to include different drugs belonging to drug groups in the drug interaction table
+CREATE TABLE Drug_Class_Interaction AS (
+SELECT i.Drug_Interaction_ID,
+	   i.Drug_1_Name,
+       COALESCE(i.Drug_1_RxCUI,g.RxNorm) AS drug_1_rxcui,
+       i.Drug_1_Class_Name,
+       i.Drug_1_Code,
+       i.Drug_2_Name,
+       COALESCE(i.Drug_2_RxCUI,h.RxNorm) AS drug_2_rxcui,
+       i.Drug_2_Class_Name,
+       i.Drug_2_Code,
+	   g.Drug_Name AS Drug_Name_1,
+       i.Drug_1_RxCUI AS Drug_1_RxNorm,
+       g.RxNorm AS Drug_1_RxNorm_Group,
        g.Class_Code AS Class_Code_1,
        h.Drug_Name AS Drug_Name_2,
-       h.RxNorm AS Drug_2_RxNorm,
-       h.Class_Code AS Class_Code_2,
-       i.*
+       i.Drug_2_RxCUI AS Drug_2_RxNorm,
+       h.RxNorm AS Drug_2_RxNorm_Group,
+       h.Class_Code AS Class_Code_2
 FROM Drug_Interaction i
 LEFT JOIN Drug_Group g
 ON (i.Drug_1_Code = TRIM(TRAILING '\r' FROM g.Class_Code))
 LEFT JOIN Drug_Group h
 ON (i.Drug_2_Code = TRIM(TRAILING '\r' FROM h.Class_Code)));
-
-SELECT * FROM(
-	SELECT * FROM (
-		SELECT drug_1_rxcui AS rxcui_1, 
-			   drug_1_rxcui AS rxnorm_1, 
-			   drug_2_rxcui AS rxcui_2, 
-			   drug_2_rxcui AS rxnorm_2
-		FROM NDF_RT_INTERACTION
-		WHERE drug_1_rxcui is not null
-		AND drug_2_rxcui is not null
-		UNION ALL
-		SELECT Drug_1_RxCUI, Drug_1_RxNorm, Drug_2_RxCUI, Drug_2_RxNorm
-		FROM Drug_Class_Interaction
-		WHERE (Drug_1_RxCUI is not null
-		OR Drug_1_RxNorm is not null)
-		AND (Drug_2_RxCUI is not null
-		OR Drug_2_RxNorm is not null)
-	) AS all_pddi 
-	GROUP BY rxcui_1, rxnorm_1, rxcui_2, rxnorm_2
-	HAVING COUNT(*) > 1) as overlap_pddi
-WHERE ((rxnorm_1 != rxnorm_2) OR (rxnorm_1 is null OR rxnorm_2 is null)) AND
-	  ((rxcui_1 != rxnorm_2) OR (rxcui_1 is null OR rxnorm_2 is null)) AND
-      ((rxnorm_1 != rxcui_2) OR (rxnorm_1 is null OR rxcui_2 is null)) AND
-      ((rxcui_1 != rxcui_2) OR (rxcui_1 is null OR rxcui_2 is null));
+      
+SELECT * FROM (
+	SELECT drug_1_rxcui,
+		   drug_2_rxcui
+	FROM NDF_RT_INTERACTION
+	WHERE drug_1_rxcui is not null
+	AND drug_2_rxcui is not null
+	UNION ALL
+	SELECT drug_1_rxcui, drug_2_rxcui
+	FROM Drug_Class_Interaction
+	WHERE drug_1_rxcui is not null 
+	AND drug_2_rxcui is not null
+) AS all_pddi 
+WHERE drug_1_rxcui != drug_2_rxcui 
+GROUP BY drug_1_rxcui, drug_2_rxcui
+HAVING COUNT(*) > 1;
