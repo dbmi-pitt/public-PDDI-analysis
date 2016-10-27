5209287317f006939438565e70c7949c0593445c
Eric Chou <echou53@gmail.com>
2016-10-27 16:36:32 -0400

    Set differences included, overlap debugged

--- PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
+++ PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
@@ -23,14 +23,19 @@ FROM Drug_Interaction i
 LEFT JOIN Drug_Group g
 ON (i.Drug_1_Code = TRIM(TRAILING '\r' FROM g.Class_Code))
 LEFT JOIN Drug_Group h
-ON (i.Drug_2_Code = TRIM(TRAILING '\r' FROM h.Class_Code)));
-      
-SELECT * FROM (
+ON (i.Drug_2_Code = TRIM(TRAILING '\r' FROM h.Class_Code))
+WHERE COALESCE(i.Drug_1_RxCUI,g.RxNorm) != COALESCE(i.Drug_2_RxCUI,h.RxNorm)
+GROUP BY COALESCE(i.Drug_1_RxCUI,g.RxNorm), COALESCE(i.Drug_2_RxCUI,h.RxNorm));
+
+# Intersect of WorldVista and NDF-RT datasets
+
+SELECT *, COUNT(*) FROM (
 	SELECT drug_1_rxcui,
 		   drug_2_rxcui
 	FROM NDF_RT_INTERACTION
 	WHERE drug_1_rxcui is not null
 	AND drug_2_rxcui is not null
+    GROUP BY drug_1_rxcui, drug_2_rxcui
 	UNION ALL
 	SELECT drug_1_rxcui, drug_2_rxcui
 	FROM Drug_Class_Interaction
@@ -39,4 +44,63 @@ SELECT * FROM (
 ) AS all_pddi 
 WHERE drug_1_rxcui != drug_2_rxcui 
 GROUP BY drug_1_rxcui, drug_2_rxcui
-HAVING COUNT(*) > 1;
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC;
+
+# Set difference of PDDI's that are only in the NDF-RT data set.
+
+SELECT * FROM NDF_RT_INTERACTION n
+LEFT JOIN(
+	SELECT * FROM (
+		SELECT drug_1_rxcui,
+			   drug_2_rxcui
+		FROM NDF_RT_INTERACTION
+		WHERE drug_1_rxcui is not null
+		AND drug_2_rxcui is not null
+        GROUP BY drug_1_rxcui, drug_2_rxcui
+		UNION ALL
+		SELECT drug_1_rxcui, drug_2_rxcui
+		FROM Drug_Class_Interaction
+		WHERE drug_1_rxcui is not null 
+		AND drug_2_rxcui is not null
+	) AS all_pddi 
+	WHERE drug_1_rxcui != drug_2_rxcui 
+	GROUP BY drug_1_rxcui, drug_2_rxcui
+	HAVING COUNT(*) > 1
+) o 
+ON o.drug_1_rxcui = n.drug_1_rxcui
+AND o.drug_2_rxcui = n.drug_2_rxcui
+WHERE o.drug_1_rxcui is null
+AND o.drug_2_rxcui is null
+AND n.drug_1_rxcui is not null
+AND n.drug_2_rxcui is not null
+GROUP BY n.drug_1_rxcui, n.drug_2_rxcui;
+
+# Set difference of PDDI's that are only in the WorldVista data set.
+
+SELECT * FROM Drug_Class_Interaction w
+LEFT JOIN(
+	SELECT * FROM (
+		SELECT drug_1_rxcui,
+			   drug_2_rxcui
+		FROM NDF_RT_INTERACTION
+		WHERE drug_1_rxcui is not null
+		AND drug_2_rxcui is not null
+        GROUP BY drug_1_rxcui, drug_2_rxcui
+		UNION ALL
+		SELECT drug_1_rxcui, drug_2_rxcui
+		FROM Drug_Class_Interaction
+		WHERE drug_1_rxcui is not null 
+		AND drug_2_rxcui is not null
+	) AS all_pddi 
+	WHERE drug_1_rxcui != drug_2_rxcui 
+	GROUP BY drug_1_rxcui, drug_2_rxcui
+	HAVING COUNT(*) > 1
+) o 
+ON o.drug_1_rxcui = w.drug_1_rxcui
+AND o.drug_2_rxcui = w.drug_2_rxcui
+WHERE o.drug_1_rxcui is null
+AND o.drug_2_rxcui is null
+AND w.drug_1_rxcui is not null
+AND w.drug_2_rxcui is not null
+GROUP BY w.drug_1_rxcui, w.drug_2_rxcui;
