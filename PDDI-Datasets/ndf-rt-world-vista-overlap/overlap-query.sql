35f9e81f01a25914010a5c3be2714d66f9f0ab77
Eric Chou <echou53@gmail.com>
2016-11-07 13:16:43 -0500

    Drug name, spreadsheet output of queries

--- PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
+++ PDDI-Datasets/ndf-rt-world-vista-overlap/overlap-query.sql
@@ -29,78 +29,101 @@ GROUP BY COALESCE(i.Drug_1_RxCUI,g.RxNorm), COALESCE(i.Drug_2_RxCUI,h.RxNorm));
 
 # Intersect of WorldVista and NDF-RT datasets
 
-SELECT *, COUNT(*) FROM (
-	SELECT drug_1_rxcui,
-		   drug_2_rxcui
-	FROM NDF_RT_INTERACTION
-	WHERE drug_1_rxcui is not null
-	AND drug_2_rxcui is not null
-    GROUP BY drug_1_rxcui, drug_2_rxcui
-	UNION ALL
-	SELECT drug_1_rxcui, drug_2_rxcui
-	FROM Drug_Class_Interaction
-	WHERE drug_1_rxcui is not null 
-	AND drug_2_rxcui is not null
-) AS all_pddi 
-WHERE drug_1_rxcui != drug_2_rxcui 
+SELECT rx1.STR, drug_1_rxcui, rx2.STR, drug_2_rxcui FROM(
+	SELECT * FROM (
+		SELECT drug_1_rxcui,
+			   drug_2_rxcui
+		FROM NDF_RT_INTERACTION
+		WHERE drug_1_rxcui is not null
+		AND drug_2_rxcui is not null
+		GROUP BY drug_1_rxcui, drug_2_rxcui
+		UNION ALL
+		SELECT drug_1_rxcui, drug_2_rxcui
+		FROM Drug_Class_Interaction
+		WHERE drug_1_rxcui is not null 
+		AND drug_2_rxcui is not null
+	) AS all_pddi 
+	WHERE drug_1_rxcui != drug_2_rxcui 
+	GROUP BY drug_1_rxcui, drug_2_rxcui
+	HAVING COUNT(*) > 1) AS overlap
+INNER JOIN rxnorm.RXNCONSO AS rx1 ON drug_1_rxcui = rx1.RXCUI
+INNER JOIN rxnorm.RXNCONSO AS rx2 ON drug_2_rxcui = rx2.RXCUI
 GROUP BY drug_1_rxcui, drug_2_rxcui
-HAVING COUNT(*) > 1
-ORDER BY COUNT(*) DESC;
+INTO OUTFILE 'worldvista-NDFRT-overlap.csv'
+FIELDS ENCLOSED BY '"'
+TERMINATED BY ','
+LINES TERMINATED BY '\n';
 
 # Set difference of PDDI's that are only in the NDF-RT data set.
 
-SELECT * FROM NDF_RT_INTERACTION n
-LEFT JOIN(
-	SELECT * FROM (
-		SELECT drug_1_rxcui,
-			   drug_2_rxcui
-		FROM NDF_RT_INTERACTION
-		WHERE drug_1_rxcui is not null
-		AND drug_2_rxcui is not null
-        GROUP BY drug_1_rxcui, drug_2_rxcui
-		UNION ALL
-		SELECT drug_1_rxcui, drug_2_rxcui
-		FROM Drug_Class_Interaction
-		WHERE drug_1_rxcui is not null 
-		AND drug_2_rxcui is not null
-	) AS all_pddi 
-	WHERE drug_1_rxcui != drug_2_rxcui 
-	GROUP BY drug_1_rxcui, drug_2_rxcui
-	HAVING COUNT(*) > 1
-) o 
-ON o.drug_1_rxcui = n.drug_1_rxcui
-AND o.drug_2_rxcui = n.drug_2_rxcui
-WHERE o.drug_1_rxcui is null
-AND o.drug_2_rxcui is null
-AND n.drug_1_rxcui is not null
-AND n.drug_2_rxcui is not null
-GROUP BY n.drug_1_rxcui, n.drug_2_rxcui;
+SELECT rx1.STR, d.drug_1_rxcui, rx2.STR, d.drug_2_rxcui FROM(
+	SELECT n.drug_1_rxcui, n.drug_2_rxcui FROM NDF_RT_INTERACTION n
+	LEFT JOIN(
+		SELECT * FROM (
+			SELECT drug_1_rxcui,
+				   drug_2_rxcui
+			FROM NDF_RT_INTERACTION
+			WHERE drug_1_rxcui is not null
+			AND drug_2_rxcui is not null
+			GROUP BY drug_1_rxcui, drug_2_rxcui
+			UNION ALL
+			SELECT drug_1_rxcui, drug_2_rxcui
+			FROM Drug_Class_Interaction
+			WHERE drug_1_rxcui is not null 
+			AND drug_2_rxcui is not null
+		) AS all_pddi 
+		WHERE drug_1_rxcui != drug_2_rxcui 
+		GROUP BY drug_1_rxcui, drug_2_rxcui
+		HAVING COUNT(*) > 1
+	) o 
+	ON o.drug_1_rxcui = n.drug_1_rxcui
+	AND o.drug_2_rxcui = n.drug_2_rxcui
+	WHERE o.drug_1_rxcui is null
+	AND o.drug_2_rxcui is null
+	AND n.drug_1_rxcui is not null
+	AND n.drug_2_rxcui is not null
+	GROUP BY n.drug_1_rxcui, n.drug_2_rxcui) AS d
+INNER JOIN rxnorm.RXNCONSO AS rx1 ON d.drug_1_rxcui = rx1.RXCUI
+INNER JOIN rxnorm.RXNCONSO AS rx2 ON d.drug_2_rxcui = rx2.RXCUI
+GROUP BY d.drug_1_rxcui, d.drug_2_rxcui
+INTO OUTFILE 'NDFRT-set-difference.csv'
+FIELDS ENCLOSED BY '"'
+TERMINATED BY ','
+LINES TERMINATED BY '\n';
 
 # Set difference of PDDI's that are only in the WorldVista data set.
 
-SELECT * FROM Drug_Class_Interaction w
-LEFT JOIN(
-	SELECT * FROM (
-		SELECT drug_1_rxcui,
-			   drug_2_rxcui
-		FROM NDF_RT_INTERACTION
-		WHERE drug_1_rxcui is not null
-		AND drug_2_rxcui is not null
-        GROUP BY drug_1_rxcui, drug_2_rxcui
-		UNION ALL
-		SELECT drug_1_rxcui, drug_2_rxcui
-		FROM Drug_Class_Interaction
-		WHERE drug_1_rxcui is not null 
-		AND drug_2_rxcui is not null
-	) AS all_pddi 
-	WHERE drug_1_rxcui != drug_2_rxcui 
-	GROUP BY drug_1_rxcui, drug_2_rxcui
-	HAVING COUNT(*) > 1
-) o 
-ON o.drug_1_rxcui = w.drug_1_rxcui
-AND o.drug_2_rxcui = w.drug_2_rxcui
-WHERE o.drug_1_rxcui is null
-AND o.drug_2_rxcui is null
-AND w.drug_1_rxcui is not null
-AND w.drug_2_rxcui is not null
-GROUP BY w.drug_1_rxcui, w.drug_2_rxcui;
+SELECT rx1.STR, d.drug_1_rxcui, rx2.STR, d.drug_2_rxcui FROM(
+	SELECT w.drug_1_rxcui, w.drug_2_rxcui FROM Drug_Class_Interaction w
+	LEFT JOIN(
+		SELECT * FROM (
+			SELECT drug_1_rxcui,
+				   drug_2_rxcui
+			FROM NDF_RT_INTERACTION
+			WHERE drug_1_rxcui is not null
+			AND drug_2_rxcui is not null
+			GROUP BY drug_1_rxcui, drug_2_rxcui
+			UNION ALL
+			SELECT drug_1_rxcui, drug_2_rxcui
+			FROM Drug_Class_Interaction
+			WHERE drug_1_rxcui is not null 
+			AND drug_2_rxcui is not null
+		) AS all_pddi 
+		WHERE drug_1_rxcui != drug_2_rxcui 
+		GROUP BY drug_1_rxcui, drug_2_rxcui
+		HAVING COUNT(*) > 1
+	) o 
+	ON o.drug_1_rxcui = w.drug_1_rxcui
+	AND o.drug_2_rxcui = w.drug_2_rxcui
+	WHERE o.drug_1_rxcui is null
+	AND o.drug_2_rxcui is null
+	AND w.drug_1_rxcui is not null
+	AND w.drug_2_rxcui is not null
+	GROUP BY w.drug_1_rxcui, w.drug_2_rxcui) AS d
+LEFT JOIN rxnorm.RXNCONSO AS rx1 ON d.drug_1_rxcui = rx1.RXCUI
+LEFT JOIN rxnorm.RXNCONSO AS rx2 ON d.drug_2_rxcui = rx2.RXCUI
+GROUP BY d.drug_1_rxcui, d.drug_2_rxcui
+INTO OUTFILE 'worldvista-set-difference.csv'
+FIELDS ENCLOSED BY '"'
+TERMINATED BY ','
+LINES TERMINATED BY '\n';
