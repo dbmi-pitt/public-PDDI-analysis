c47738172691efbd2508ac250ee71f88d05dd878
Eric Chou <echou53@gmail.com>
2016-12-21 15:50:34 -0500

    Adjusted null update for new csv file

--- PDDI-Datasets/ndf-rt-world-vista-overlap/ndf-rt-schema.sql
+++ PDDI-Datasets/ndf-rt-world-vista-overlap/ndf-rt-schema.sql
@@ -12,18 +12,18 @@ CREATE TABLE NDF_RT_INTERACTION (
 
 LOAD DATA LOCAL INFILE './NDF-RT-interactions.csv' INTO TABLE NDF_RT_INTERACTION FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (drug_interaction_id, drug_1_vuid, drug_2_vuid, severity, drug_1_rxcui, drug_2_rxcui);
 
-UPDATE NDF_RT_INTERACTION
-SET drug_2_rxcui = TRIM(TRAILING '\r' FROM drug_2_rxcui);
-
 SET SQL_SAFE_UPDATES=0;
 
 UPDATE NDF_RT_INTERACTION
+SET drug_2_rxcui = TRIM(TRAILING '\r' FROM drug_2_rxcui);
+
+UPDATE NDF_RT_INTERACTION
 SET drug_1_rxcui = NULL
-WHERE drug_1_rxcui = 'NULL';
+WHERE drug_1_rxcui = '';
 
 UPDATE NDF_RT_INTERACTION
 SET drug_2_rxcui = NULL
-WHERE drug_2_rxcui = 'NULL';
+WHERE drug_2_rxcui = '';
 
 # 269527 in data set total, 67 duplicates need to be removed => 269460
 
